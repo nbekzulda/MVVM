af99a4da553c9835bc828d454b15ee497677c92d
Nursultan <nbekzulda@gmail.com>
2020-06-17 18:21:04 +0600

    long text

---
+++ EmotionalFace/GamiSoftCustomView/app/src/main/java/com/example/gamisoftcustomview/ItemRating.kt
@@ -0,0 +1,66 @@
+package com.example.gamisoftcustomview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ItemRating : ConstraintLayout {
+
+    private lateinit var tvSectionName: TextView
+    private lateinit var tvRatingValue: TextView
+    private lateinit var ivForwardArrow: ImageView
+
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    fun getArrow(): ImageView {
+        return ivForwardArrow
+    }
+
+    fun setSectionName(name: String): TextView {
+        tvSectionName.text = name
+        return tvSectionName
+    }
+
+    fun setRatingValue(rating: String): TextView {
+        tvRatingValue.text = rating
+        return tvRatingValue
+    }
+
+    private fun init(context: Context){
+        LayoutInflater.from(context)
+            .inflate(R.layout.item_rating, this)
+
+        tvSectionName = findViewById(R.id.sectionName)
+        tvRatingValue = findViewById(R.id.ratingValue)
+        ivForwardArrow = findViewById(R.id.ivForward)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet) {
+        init(context)
+
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ItemRating)
+
+        tvSectionName.apply {
+            text = typedArray.getText(R.styleable.ItemRating_sectionName)
+        }
+
+        tvRatingValue.apply {
+            text = typedArray.getText(R.styleable.ItemRating_ratingValue)
+        }
+
+        typedArray.recycle()
+    }
+
+
+}
---
+++ EmotionalFace/GamiSoftCustomView/app/src/main/java/com/example/gamisoftcustomview/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.gamisoftcustomview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var itemRating: ItemRating
+ 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        itemRating = findViewById(R.id.itemRating)
+
+        itemRating.setSectionName("Обратите внимание: в имени пакета используется схема «обратного DNS», согласно которой доменное имя вашей организации записывается в обратном порядке с присоединением суффиксов дополнительных идентификаторов. Эта схема обеспечивает уникальность имен пакетов и позволяет различать приложения на устройстве и в Google Play. ")
+        itemRating.setRatingValue("8800000000000 результатов")
+        itemRating.getArrow()
+    }
+}
